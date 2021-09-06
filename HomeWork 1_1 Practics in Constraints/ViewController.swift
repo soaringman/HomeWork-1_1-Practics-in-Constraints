d03a41a95e8bd068691899a11605057b42b5640f
Alexey Gulyaev <ghost-shadows@yandex.ru>
2021-09-06 20:39:52 +0300

    Initial Commit

---
+++ HomeWork 1_1 Practics in Constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork 1_1 Practics in Constraints
+//
+//  Created by Алексей Гуляев on 06.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
